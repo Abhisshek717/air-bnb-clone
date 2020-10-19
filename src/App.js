ffbc6a879558df29e9c4bed24aa7083fd591bc07
Abhishek <46296076+AbhisshekCP@users.noreply.github.com>
2020-10-20 02:41:28 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import Home from './Home'
+import Header from './Header'
+function App() {
+  return (
+    <div className="App">
+       <Header/> 
+       <Home/>
+           {/*header*/}
+           {/*banner*/}
+           {/*cards*/}
+           {/*footer*/}
+
+
+        {/*search page*/}
+            {/*Header*/}
+            {/*.......*/}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Home.css'
+function Home() {
+    return (
+        <div className="home">
+           
+        </div>
+    )
+}
+
+export default Home
