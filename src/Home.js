45cc5787b8019e5756f52ab3ad3581ebcbe5a448
Abhishek <46296076+AbhisshekCP@users.noreply.github.com>
2020-10-21 01:32:55 +0530

    added banner component into home.js

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import './Home.css'
+import Banner from './Banner'
 function Home() {
     return (
         <div className="home">
-           
+           <Banner/>
         </div>
     )
 }
