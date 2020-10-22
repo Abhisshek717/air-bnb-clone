7d8ff6a579e47afb8d0638f2e4356032c768e909
Abhishek <46296076+AbhisshekCP@users.noreply.github.com>
2020-10-23 01:03:02 +0530

    updated App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,35 @@ import React from 'react';
 import './App.css';
 import Home from './Home'
 import Header from './Header'
+import Footer from './Footer'
+import SearchPage from './SearchPage';
+
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
+    
+    <Router>
+
        <Header/> 
-       <Home/>
+
+       <Switch>
+         <Route path="/search">
+       <SearchPage/>
+         </Route>
+
+       <Route path="/">
+         <Home/>
+       </Route>
+       
+       
+       </Switch>
+       
+       <Footer/>
+
+
+     </Router>  
            {/*header*/}
            {/*banner*/}
            {/*cards*/}
