456b401affa5155b5525e2b6747d755386960b48
Abhishek <46296076+AbhisshekCP@users.noreply.github.com>
2020-10-23 01:04:09 +0530

    updated Banner.js

--- src/Banner.js
+++ src/Banner.js
@@ -1,16 +1,23 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './Banner.css'
 import { Button } from "@material-ui/core";
+import Search from './Search';
+import { useHistory } from "react-router-dom";
 function Banner() {
+    const history = useHistory();
+    const[showSearch,setShowSearch] = useState(false);
+
+
     return (
         <div className="banner">
             <div className="banner__search">
-                <Button className="banner__searchButton" variant="outlined">Search Dates</Button>
+                {showSearch && <Search/>}
+                <Button onClick={() => setShowSearch(!showSearch)} className="banner__searchButton" variant="outlined">{showSearch ? "Hide" : "Search Dates"}</Button>
             </div>
             <div className="banner__info">
                 <h1>Get out and streach your imagination</h1>
                 <h5>Plan a different kind of getaway to uncover the hidden gems near you</h5>
-                <Button variant="outlined">Explore Nearby</Button>
+                <Button onClick={() => history.push('/search')} variant="outlined">Explore Nearby</Button>
             </div>
         </div>
     )
